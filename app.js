fbc92a2e75e228912015f16936e55925b35d2ef2
Jordon Cunningham <153477169+JAC874@users.noreply.github.com>
2024-10-23 13:23:35 -0400

    fresh commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { errors } = require("celebrate");
 const app = express();
 const { PORT = 3001 } = process.env;
 const errorHandler = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
@@ -18,6 +19,12 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use("/", indexRouter);
+
+app.use(requestLogger);
+app.use(routes);
+
+app.use(errorLogger);
+
 app.use(errors());
 app.use(errorHandler);
 
